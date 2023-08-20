b98dacbd580beac6b99171225ab7593a533ad97a
luhoro <luisashorowitz@gmail.com>
2023-08-20 11:37:10 -0300

    refactor escape decorator

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -4,18 +4,13 @@ import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js"
 export abstract class View<Type> {
 
   protected elemento: HTMLElement
-  private escapar = false
 
-  constructor(seletor: string, escapar?: boolean) {
+  constructor(seletor: string) {
     const elemento = document.querySelector(seletor)
     if (elemento) {
       this.elemento = elemento as HTMLElement
     } else {
       throw Error(`Seletor ${seletor}, não existe no DOM`)
-    }
-
-    if (escapar) {
-      this.escapar = escapar
     }
   }
 
@@ -23,10 +18,6 @@ export abstract class View<Type> {
   @inspect()
   public update(model: Type): void {
     let template = this.template(model)
-    if (this.escapar) {
-      template = template
-        .replace(/<script>[\s\S]*?<\/script>/, '')
-    }
     this.elemento.innerHTML = template
   }
 
